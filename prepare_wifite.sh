9e65f9dad2f94d0e74c28845b8116da548bbe18f
Przemysław Szmaj <37410338+PSZMAJ@users.noreply.github.com>
2022-04-05 17:02:42 +0200

    Update prepare_wifite.sh

--- prepare_wifite.sh
+++ prepare_wifite.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "Przygotowanie narzędzia Wifite do działania... Za chwilę rozpocznie się instalacja."
-sleep 2
-echo -e "\033[32;40m"
 echo "www.ehaker.pl"
-sleep 4
+sleep 2
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install python3-pip  
+sudo dpkg --configure -a
 sudo apt-get install hcxdumptool && sudo apt-get install hcxtools
 git clone https://github.com/hacker3983/pyrit-installer && cd pyrit-installer && sudo bash install.sh
 echo "Instalacja zakończona."
-sudo wifite
+sudo wifite --kill
